77b47d06052866b1f5cd5b2b3a4d8694e8336e92
Rajanikant Deshmukh <rajanikantdeshmukh@gmail.com>
2015-08-10 02:05:17 +0530

    Add basic colors.

---
+++ colors.py
@@ -0,0 +1,8 @@
+# Module for containing colors
+WHITE       = (255, 255, 255)
+BLACK       = (  0,   0,   0)
+RED         = (255,   0,   0)
+GREEN       = (  0, 255,   0)
+DARKGREEN   = (  0, 155,   0)
+DARKGRAY    = ( 40,  40,  40)
+BGCOLOR = BLACK
