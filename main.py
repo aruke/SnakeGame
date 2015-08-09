aa49a7015eaa0d9d18f298ac449ac0d2333f17f6
Rajanikant Deshmukh <rajanikantdeshmukh@gmail.com>
2015-08-10 02:24:12 +0530

    Add draw_grid function.

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ try :
     import sys
     import random
     from pygame.locals import *
+    from colors import *
 except ImportError :
     print "Cannot import required module(s). Abort."
 
 FPS = 15
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
+CELL_SIZE = 20
+
+assert WINDOW_WIDTH % CELL_SIZE == 0 , "WINDOW_WIDTH must be a multiple of CELL_SIZE"
+assert WINDOW_HEIGHT % CELL_SIZE == 0 , "WINDOW_HEIGHT must be a multiple of CELL_SIZE"
+
+# Draw Functions
+
+def draw_grid():
+    for x in range(0, WINDOW_WIDTH, CELL_SIZE):
+        pygame.draw.line(DISPLAY, DARKGRAY, (x,0), (x,WINDOW_HEIGHT))
+    for y in range(0, WINDOW_HEIGHT, CELL_SIZE):
+        pygame.draw.line(DISPLAY, DARKGRAY, (0,y), (WINDOW_WIDTH,y))
+
+# main.py
 
 def main():
     global FPS_CLOCK, DISPLAY, BASIC_FONT
@@ -21,7 +36,9 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+        draw_grid()
         pygame.display.update()
+        
 
 if __name__=='__main__':
     main()
